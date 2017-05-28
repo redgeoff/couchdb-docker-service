f6e39cd073ae639253188ae752e58f243f2a5311
Geoff Cox <redgeoff@gmail.com>
2017-05-27 22:28:57 -0700

    fix(discover-process): service name

--- discover-process.sh
+++ discover-process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Wait until docker has finished setting up /etc/hosts
-/wait-for-host.sh ${$SERVICE_NAME}${TASK_SLOT}
+/wait-for-host.sh ${SERVICE_NAME}${TASK_SLOT}
 
 docker-discover-tasks -s ${SERVICE_NAME} -p 3000 > /var/log/docker-discover-tasks.log
